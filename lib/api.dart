190c03013dfe75b254ff639cd93818ec7e7be25f
bladerunner2912 <bladerunner2912@gmail.com>
2023-03-22 14:09:30 +0530

    permission and version changes  in build gradle

--- lib/api.dart
+++ lib/api.dart
@@ -1,20 +1,19 @@
 var webApi = {
-  // 'domain': 'https://heda-saathi.onrender.com' //prod
-  'domain': 'https://530e-182-48-222-223.in.ngrok.io' //dev
-}; 
+  'domain': 'https://heda-saathi.onrender.com' //prod
+  // 'domain': 'https://530e-182-48-222-223.in.ngrok.io' //dev
+};
 
 var endPoints = {
-  
   //Auth Modulek
   'loginUser': 'users/loginUser',
-  'searchMembers' : 'users/searchMembers',
-  'fetchMembers' : 'users/fetchMembers',
-  
+  'searchMembers': 'users/searchMembers',
+  'fetchMembers': 'users/fetchMembers',
+
   //User Module
-  
+
   //Feature Module
 
   //Notification Module
 
   //Chat Module
-  };
+};
